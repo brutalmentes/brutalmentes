8435c04a3da35a65030a450ea003b37b34488d45
Davi Pinheiro Viana <davipviana@gmail.com>
2016-05-05 20:48:32 -0300

    Qualquer coisa 2

--- movimentacao/lib/dot.h
+++ movimentacao/lib/dot.h
@@ -1,7 +1,26 @@
-#ifndef LTEXTURE_H
-#define LTEXTURE_H
+#ifndef DOT_H
+#define DOT_H
 
 #include "share.h"
 
+class Dot {
+	public:
+		static const int DOT_WIDTH = 20;
+		static const int DOT_HEIGHT = 20;
+
+		static const int DOT_VEL = 1;
+
+		Dot();
+
+		void handleEvent(SDL_Event& e);
+
+		void move();
+
+		void render(LTexture* gDotTexture, LTexture* backgroundTexture, SDL_Renderer *gRenderer);
+	private:
+		int mPosX, mPosY;
+
+		int mVelX, mVelY;
+};
 
 #endif
--- movimentacao/main.cpp
+++ movimentacao/main.cpp
@@ -1,5 +1,6 @@
 //Using SDL, SDL_image, standard IO, and strings
 #include "lib/ltexture.h"
+#include "lib/dot.h"
 
 SDL_Renderer* gRenderer = NULL;
 
@@ -14,70 +15,7 @@ SDL_Window* gWindow = NULL;
 LTexture gDotTexture;
 LTexture backgroundTexture;
 
-class Dot {
-	public:
-		static const int DOT_WIDTH = 20;
-		static const int DOT_HEIGHT = 20;
 
-		static const int DOT_VEL = 1;
-
-		Dot();
-
-		void handleEvent(SDL_Event& e);
-
-		void move();
-
-		void render();
-	private:
-		int mPosX, mPosY;
-
-		int mVelX, mVelY;
-};
-
-Dot::Dot() {
-	mPosX = 0;
-	mPosY = 0;
-
-	mVelX = 0;
-	mVelY = 0;
-}
-
-void Dot::handleEvent(SDL_Event& e) {
-	if(e.type == SDL_KEYDOWN && e.key.repeat == 0) {
-		switch(e.key.keysym.sym) {
-			case SDLK_UP: mVelY -= DOT_VEL; break;
-			case SDLK_DOWN: mVelY += DOT_VEL; break;
-			case SDLK_LEFT: mVelX -= DOT_VEL; break;
-			case SDLK_RIGHT: mVelX += DOT_VEL; break;
-		}
-	} else if(e.type == SDL_KEYUP && e.key.repeat == 0) {
-		switch(e.key.keysym.sym) {
-			case SDLK_UP: mVelY += DOT_VEL; break;
-			case SDLK_DOWN: mVelY -= DOT_VEL; break;
-			case SDLK_LEFT: mVelX += DOT_VEL; break;
-			case SDLK_RIGHT: mVelX -= DOT_VEL; break;
-		}
-	}
-}
-
-void Dot::move() {
-	mPosX += mVelX;
-
-	if((mPosX < 0) || (mPosX + DOT_WIDTH > SCREEN_WIDTH)) {
-		mPosX -= mVelX;
-	}
-
-	mPosY += mVelY;
-
-	if((mPosY < 0) || (mPosY + DOT_HEIGHT > SCREEN_HEIGHT)) {
-		mPosY -= mVelY;
-	}
-}
-
-void Dot::render() {
-	backgroundTexture.render(gRenderer, 0,0);
-	gDotTexture.render(gRenderer, mPosX, mPosY);
-}
 
 bool init() {
 	bool success = true;
@@ -166,7 +104,7 @@ int main( int argc, char* args[] ) {
 
 				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 				SDL_RenderClear(gRenderer);
-				dot.render();
+				dot.render(&backgroundTexture, &gDotTexture, gRenderer);
 				SDL_RenderPresent( gRenderer );
 			}
 		}
